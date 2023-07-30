c49111ab4029f37aa21358af43e506a71d071547
Cakra Danu Sedayu <cakra.ds@gmail.com>
2023-07-30 16:41:55 +0700

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	export let name = 'test';
+</script>
+
+<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation {name}</p>
