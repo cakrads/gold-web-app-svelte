5ea91925ccfa03477c62eb9e59113b9b26838c35
Cakra Danu Sedayu <cakra.ds@gmail.com>
2023-08-07 10:04:46 +0700

    feat: add nav component

---
+++ src/components/nav/nav.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Text from '$components/atoms/typography/text.svelte';
+	import Theme from '$lib/Theme.svelte';
+</script>
+
+<nav class="flex justify-between h-16 px-6">
+	<div class="flex">
+		<div class="flex-shrink-0 flex items-center">
+			<Text color="primary" type="title">GD</Text>
+		</div>
+	</div>
+	<div class="flex items-center">
+		<Theme variant="normal" />
+	</div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,7 @@
 </script>
 
 <script lang="ts">
-	import Theme from '$lib/Theme.svelte';
-	import Text from '$components/atoms/typography/text.svelte';
+	import Nav from '$components/nav/nav.svelte';
 </script>
 
 <section
@@ -13,16 +12,7 @@
 	bg-main-bg-light dark:bg-black text-main-text-light dark:text-white
 	"
 >
-	<nav class="flex justify-between h-16 px-6">
-		<div class="flex">
-			<div class="flex-shrink-0 flex items-center">
-				<Text color="primary" type="title">GD</Text>
-			</div>
-		</div>
-		<div class="flex items-center">
-			<Theme variant="normal" />
-		</div>
-	</nav>
+	<Nav />
 	<main>
 		<slot />
 	</main>
