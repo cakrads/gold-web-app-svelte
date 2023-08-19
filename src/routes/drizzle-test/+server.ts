ad3c1d065af2ccd540388f65ac3ecbd4ce59cde8
Cakra Danu Sedayu <cakra.ds@gmail.com>
2023-08-19 19:25:51 +0700

    config: just playcort for test drizzle

---
+++ src/routes/drizzle-test/+server.ts
@@ -0,0 +1,9 @@
+import DailyInfoService from "$lib/services/daily-info";
+import type { DailyInfo } from "$lib/services/daily-info";
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async () => {
+  const mainInfo = await DailyInfoService.getByDate(1691989961) || {} as DailyInfo;
+  return json(mainInfo);
+};
