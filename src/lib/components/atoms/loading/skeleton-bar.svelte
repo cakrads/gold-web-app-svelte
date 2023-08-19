573e872f40c765fbf9354752b2abcf4d77593bf4
Cakra Danu Sedayu <cakra.ds@gmail.com>
2023-08-19 19:20:45 +0700

    feat: add skeleton component

---
+++ src/lib/components/atoms/loading/skeleton-bar.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { twMerge } from 'tailwind-merge';
+
+	export let className: string = '';
+</script>
+
+<div class={twMerge('h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full mb-2', className)} />
---
+++ src/lib/components/atoms/loading/skeleton-list.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { twMerge } from 'tailwind-merge';
+	import SkeletonBar from './skeleton-bar.svelte';
+
+	export let listLength: number = 5;
+	export let className: string = '';
+
+	const newArray = new Array(listLength);
+</script>
+
+<div class="animate-pulse">
+	{#each newArray as _}
+		<SkeletonBar className={twMerge('h-14 mb-[1px] rounded-none', className)} />
+	{/each}
+</div>
