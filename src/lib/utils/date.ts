0bdd781a88bc9af9542fb40af680681766c8928b
Cakra Danu Sedayu <cakra.ds@gmail.com>
2023-08-24 22:45:15 +0700

    feat: add convert date simple

--- src/lib/utils/date.ts
+++ src/lib/utils/date.ts
@@ -1,9 +1,24 @@
+export const convertToDate = (epoch: number): Date => {
+	const epochStr = epoch.toString();
+	if (/^\d{10}$/.test(epochStr)) {
+		return new Date(epoch * 1000);
+	} else if (/^\d{13}$/.test(epochStr)) {
+		return new Date(epoch);
+	} else {
+		throw new Error('Invalid epoch timestamp');
+	}
+};
+
 export const convertDate = (epoch: number, type: 'id-ID' | 'en-US' = 'id-ID'): string => {
-	const date = new Date(epoch * 1000); // Convert epoch to milliseconds
-	if (type === 'id-ID') {
-		return convertDateIndo(date);
-	} else if (type === 'en-US') {
-		return convertDateEn(date);
+	const date = convertToDate(epoch);
+	const options = new Map([
+		['id-ID', convertDateIndo],
+		['en-US', convertDateEn],
+	]);
+
+	const selectedFunction = options.get(type);
+	if (selectedFunction) {
+		return selectedFunction(date);
 	} else {
 		throw new Error(`Invalid type. Supported types are 'ID' and 'EN'.`);
 	}
@@ -32,4 +47,39 @@ const convertDateEn = (date: Date): string => {
 	};
 
 	return date.toLocaleString('en-US', options).replace(' at', ',');
+};
+
+export const convertSimpleDate = (epoch: number, type: 'id-ID' | 'en-US' = 'id-ID'): string => {
+	const date = convertToDate(epoch);
+	const options = new Map([
+		['id-ID', convertDateSimpleIndo],
+		['en-US', convertDateSimpleEn],
+	]);
+
+	const selectedFunction = options.get(type);
+	if (selectedFunction) {
+		return selectedFunction(date);
+	} else {
+		throw new Error(`Invalid type. Supported types are 'ID' and 'EN'.`);
+	}
+};
+
+const convertDateSimpleIndo = (date: Date): string => {
+	const options: Intl.DateTimeFormatOptions = {
+		day: 'numeric',
+		month: '2-digit',
+		year: '2-digit',
+	};
+
+	return date.toLocaleString('id-ID', options);
+};
+
+const convertDateSimpleEn = (date: Date): string => {
+	const options: Intl.DateTimeFormatOptions = {
+		month: '2-digit',
+		day: '2-digit',
+		year: '2-digit',
+	};
+
+	return date.toLocaleString('en-US', options);
 };
